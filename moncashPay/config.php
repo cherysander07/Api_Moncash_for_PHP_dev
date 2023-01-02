932b236bba5b4bcd96d945dd754c3af17d84cea4
cherysander07 <cherysander@gmail.com>
2023-01-01 13:46:17 -1100

    v1.0.0

--- moncashPay/config.php
+++ moncashPay/config.php
@@ -1,6 +1,6 @@
 <?php
-$clientId = '9d1c95dd90b9ae8bc1fb038ea03dd352';
-$clientSecret = 'oHrr4tbnB1PH0uz6VQNUvaCIJIx4LuEjVKQ5IYuuCBovNJNVa-ltU0qadLAntwrV';
+$clientId = '0909g-09f-0g9fdgjfljgfjg0909g09fdj';
+$clientSecret = 'fjdjgggfgjdlfg890gg8908fgjkdflgjkhk4hkl6hlkkjljlkj55.dfklgjfdlkj';
 $isLive = false; // add true if you're in production
 $returnUrlBackHome = 'productPage.php'; // you can add your index page or a thank you page
  $userID = 1; // add user ID with cookie or session $_COOKIE['id'],
